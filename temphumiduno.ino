44d58b84aec64026fe9df170c73b616be5ec046b
Frank Lazzarini <flazzarini@gmail.com>
2014-04-06 14:36:47 +0200

    Added Sevensegement and Ethernet server

--- temphumiduno.ino
+++ temphumiduno.ino
@@ -1,5 +1,29 @@
+/*
+    Condinduino
+
+    Analyses temperature/humidity saves it to an SD Card and gives the user the
+    possibility the output the readings to a seven segment display by pressing
+    a button.
+
+
+    Wires
+
+    Adruino  ----------------------- 7-Segemnt I2C
+    5V       ----------------------- VCC
+    GND      ----------------------- GND
+    A4 (SDA) ----------------------- SDA
+    A5 (SCL) ----------------------- SCL
+
+    Arduino  ----------------------- DHT22 Sensor
+    5V       -----------\/---------- LPin1
+    PIN2     -----------/\---------- LPin2
+    GND      ----------------------- LPin4
+*/
+
 #include <dht.h>
 #include <SdFat.h>
+#include <Wire.h>
+#include <Ethernet.h>
 
 SdFat SD;
 SdFile dataFile;
@@ -9,9 +33,80 @@ const int DHT_PIN = 2;
 const int SD_PIN1 = 4;
 const int SD_PIN2 = 10;
 const int LED_PIN = 13;
+const byte S7ADDR = 0x71;
 
 int counter = 0;
-int led_switch = 0;
+int ticker = 120;       // 120 * 5sec = 10min
+int led_state = LOW;
+char temp_string[10];
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+EthernetServer server = EthernetServer(4949);
+
+/**
+ * clearDisplay
+ *  Send clear display command via I2C
+ */
+void clearDisplay() {
+    Wire.beginTransmission(S7ADDR);
+    Wire.write(0x76); // Clear display command
+    Wire.endTransmission();
+}
+
+
+/**
+ * setBrightness
+ *  Set brightness of the seven segment display
+ *  (0-127-255) (dimmest-brightest)
+ */
+void setBrightness(byte value)
+{
+  Wire.beginTransmission(S7ADDR);
+  Wire.write(0x7A);  // Set brightness command byte
+  Wire.write(value);  // brightness data byte
+  Wire.endTransmission();
+}
+
+/**
+ * setDecimals
+ *  Activates the decimal place on the Seven Segment Display
+ *   Digits go from left to right
+ *  [MSB] (X)(X)(Apos)(Colon)(Digit 4)(Digit 3)(Digit2)(Digit1)
+ */
+void setDecimals(byte decimals) {
+    Wire.beginTransmission(S7ADDR);
+    Wire.write(0x77);
+    Wire.write(decimals);
+    Wire.endTransmission();
+}
+
+/**
+ * sendString
+ *  Sends a 4 character string to the 7Segment Display
+ */
+void sendString(String toSend) {
+    Wire.beginTransmission(S7ADDR);
+    for (int i=0; i<5; i++) {
+        if (toSend[i] != '.') {
+            Wire.write(toSend[i]);
+        }
+    }
+    Wire.endTransmission();
+}
+
+/**
+ * blinkLed
+ * Will activate or deactivate PIN13 LED depending on its current
+ * state
+ */
+void blinkLed() {
+    if (led_state == LOW) {
+        led_state = HIGH;
+    } else {
+        led_state = LOW;
+    }
+    digitalWrite(LED_PIN, led_state);
+}
+
 
 
 void setup() {
@@ -19,6 +114,12 @@ void setup() {
     Serial.print("DHT22 Monitor (");
     Serial.print(DHT_LIB_VERSION);
     Serial.println(")");
+
+    // Initialize I2C Pins
+    Wire.begin();
+    clearDisplay();
+    setBrightness(127);
+    setDecimals(0b000010);  // Set decimal on position 1 on
 
     // Initialize LED on PIN 13
     pinMode(LED_PIN, OUTPUT);
@@ -44,12 +145,16 @@ void setup() {
     if (lastCounter) {
         counter = lastCounter.toInt() + 1;
     }
+
+    // Ethernet
+    Ethernet.begin(mac);
+    server.begin();
+    Serial.print("Server running at ");
+    Serial.print(Ethernet.localIP());
+    Serial.println(" on port 4949");
 }
 
 void loop() {
-    // LED Activate
-    digitalWrite(LED_PIN, HIGH);
-
     // Read DHT Data
     int chk = DHT.read22(DHT_PIN);
     switch (chk)
@@ -78,7 +183,6 @@ void loop() {
     Serial.print(" Humidity: ");
     Serial.println(humi, 1);
 
-
     // Write Date to SD
     String dataString = "";
 
@@ -87,21 +191,38 @@ void loop() {
     dtostrf(temp, 1, 2, tempString);
     dtostrf(humi, 1, 2, humiString);
 
-    dataString += String(counter);
-    dataString += ", ";
-    dataString += String(tempString);
-    dataString += ", ";
-    dataString += String(humiString);
-    Serial.print("Writing to SD Card: ");
-    Serial.println(dataString);
+    // Send String to Sevensegment
+    if ((ticker % 2) == 0) {
+        sendString(tempString);
+    } else {
+        sendString(humiString);
+    }
 
-    dataFile.open("temphumi.txt", O_RDWR | O_CREAT | O_AT_END);
-    dataFile.println(dataString);
-    dataFile.close();
+    EthernetClient client = server.available();
+    if (client) {
+        char c = client.read();
+        client.print("temperature.now ");
+        client.println(tempString);
+        client.print("humidity.now ");
+        client.println(humiString);
+    }
 
-    // LED Activate
-    digitalWrite(LED_PIN, LOW);
+    // Write to SD
+    if (ticker == 120) {
+        dataString += String(counter);
+        dataString += ", ";
+        dataString += String(tempString);
+        dataString += ", ";
+        dataString += String(humiString);
+        Serial.print("Writing to SD Card: ");
+        Serial.println(dataString);
+
+        dataFile.open("temphumi.txt", O_RDWR | O_CREAT | O_AT_END);
+        dataFile.println(dataString);
+        dataFile.close();
+    }
 
     counter++;
-    delay(600000);  // Delay 10 mins
+    ticker++;
+    delay(5000);  // Delay 5 second
 }
